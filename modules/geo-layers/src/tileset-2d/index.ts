7a674ee493a9e929cada6eddecfa2d82df7b6f00
Adam <ad@adamthomann.com>
2026-07-04 17:01:04 -0400

    feat(geo-layers): add CRSTileset2D for TileMatrixSet indexing in CRS views
    (cherry picked from commit d465455fd335c5b7d820bbee093098b2c0f07d70)

--- modules/geo-layers/src/tileset-2d/index.ts
+++ modules/geo-layers/src/tileset-2d/index.ts
@@ -18,6 +18,16 @@ export {Tileset2D, STRATEGY_DEFAULT} from './tileset-2d';
 
 export {Tile2DHeader} from './tile-2d-header';
 
+export {CRSTileset2D} from './crs-tileset-2d';
+export type {CRSTileset2DProps, CRSTileIndex} from './crs-tileset-2d';
+export {normalizeTileMatrixSet} from './tile-matrix-set';
+export type {
+  TileMatrixSet,
+  TileMatrix,
+  NormalizedTileMatrixSet,
+  NormalizedTileMatrix
+} from './tile-matrix-set';
+
 export type {URLTemplate} from './utils';
 export {
   isGeoBoundingBox,
